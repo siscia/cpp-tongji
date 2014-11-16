cfa7884ec9a2681467d975a242d3206c1018bbed
siscia <simone@mweb.biz>
2014-11-16 05:42:16 +0100

    first

---
+++ cannon.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <allegro5/allegro.h>
+
+class Bullet {  
+public:
+  float x, y;
+  float direction[2];
+  float acceleration[2];
+  Bullet(float x_in, float y_in){
+    x = x_in; y = y_in;
+  }
+  Bullet(float x_in, float y_in, float dir[2]){
+    x = x_in; y = y_in;
+    direction[0] = dir[0]; direction[1] = dir[1];
+  }
+  Bullet(float x_in, float y_in, float dir[2], float acc[2]){
+    x = x_in; y = y_in;
+    direction[0] = dir[0]; direction[1] = dir[1];
+    acceleration[0] = acc[0]; acceleration[1] = acc[1];
+  }
+  void accelerate () {
+    direction[0] += acceleration[0];
+    direction[1] += acceleration[1];
+  }
+  void move(){
+    x += direction[0];
+    y += direction[1];
+  }
+  void tick(){
+    this->accelerate();
+    this->move();
+  }
+  void display(){
+    std::cout << "Position:     [" << this->x << ", " << this->y << " ]\n";
+    std::cout << "Direction:    [ " << this->direction[0] << ", " << this->direction[1] << " ]\n";
+    std::cout << "Acceleration: [ " << this->acceleration[0] << ", " << this->acceleration[1] << " ]\n";
+  }
+};
+
+class VisualBullet : public Bullet {
+public:
+  ALLEGRO_BITMAP *bmp_p = NULL;
+  VisualBullet(float x_in, float y_in) : Bullet(x_in, y_in) {
+    ALLEGRO_DISPLAY *old = al_get_current_display();
+    bmp_p = al_create_bitmap(10, 10);
+    al_set_target_bitmap(bmp_p);
+    al_clear_to_color(al_map_rgb(255, 0, 255)); //viola
+    al_set_target_backbuffer(old);
+  }
+  VisualBullet(float x_in, float y_in, float dir[2]) : Bullet(x_in, y_in, dir) {
+    ALLEGRO_DISPLAY *old = al_get_current_display();
+    bmp_p = al_create_bitmap(10, 10);
+    al_set_target_bitmap(bmp_p);
+    al_clear_to_color(al_map_rgb(255, 0, 255)); //viola
+    al_set_target_backbuffer(old);
+  }
+  VisualBullet(float x_in, float y_in, float dir[2], float acc[2]) : Bullet(x_in, y_in, dir, acc) {
+    ALLEGRO_DISPLAY *old = al_get_current_display();
+    bmp_p = al_create_bitmap(10, 10);
+    al_set_target_bitmap(bmp_p);
+    al_clear_to_color(al_map_rgb(255, 0, 255)); //viola
+    al_set_target_backbuffer(old);
+  }
+  void draw(){
+    al_draw_bitmap(this->bmp_p, this->x, this->y, 0);
+  }
+};
+
+ALLEGRO_DISPLAY *display = NULL;
+
+int main(){
+  al_init();
+
+  float direction[2] = {2.0, 3.0};
+  float acceleration[2] = {5.0, 10.8};
+
+  VisualBullet b1 (10, 20, direction, acceleration);
+ 
+
+  display = al_create_display(640, 480);
+  
+  al_set_target_bitmap(al_get_backbuffer(display));
+
+  for (int i=0; i<20; i++){
+    b1.draw();
+    b1.tick();
+    al_flip_display();
+    al_rest(0.1);
+  }
+  al_rest(1.0);
+  al_destroy_display(display);
+  return 1;
+}
